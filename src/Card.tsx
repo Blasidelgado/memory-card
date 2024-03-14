2c779e336253157f94cd0676dcdc9ccd7e247f7e
Blasidelgado <blasidelgado@gmail.com>
2024-03-14 13:25:07 -0300

    Add minimalistic effects to cards

--- src/Card.tsx
+++ src/Card.tsx
@@ -8,16 +8,11 @@ interface CardProps {
 export default function Card({ data, onClick }: CardProps) {
   return (
     <article
-      className="m-2 w-full max-w-xs"
-      onClick={() => onClick(data.id)}
+    className=" cursor-pointer m-2 w-full max-w-xsrounded-lg overflow-hidden shadow-lg transition duration-300 transform hover:shadow-xl hover:-translate-y-1 hover:scale-105"     
+    onClick={() => onClick(data.id)}
     >
-      <img 
-        className="w-full"
-        src={data.imageUrl} 
-        alt={data.name} 
-      />
-      <p className="font-bold text-xl my-4 text-center">{data.name}</p> {/* Adjust margin for larger cards */}
+      <img className="w-full" src={data.imageUrl} alt={data.name} />
+      <p className="font-bold text-xl my-4 text-center">{data.name}</p>
     </article>
   );
 }
-
