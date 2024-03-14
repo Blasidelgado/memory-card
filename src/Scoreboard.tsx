7bc25c7d44d49315558f26b8c54597e84de73c5a
Blasidelgado <blasidelgado@gmail.com>
2024-03-14 13:34:27 -0300

    Add scoreboard props

--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -1,4 +1,9 @@
-export default function Scoreboard({ score, maxScore }) {
+interface ScoreboardProps {
+    score: number
+    maxScore: number
+}
+
+export default function Scoreboard({ score, maxScore }: ScoreboardProps) {
   return (
     <section>
       <h2>SCORE: {score}</h2>
